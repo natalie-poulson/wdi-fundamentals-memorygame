42626ad759ff5cf1e15c6ee6fbcfbd9cc08af238
Natalie Poulson <np@Natalies-MacBook-Pro.local>
2018-07-19 10:33:52 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -31,11 +31,13 @@ console.log("Sorry, try again.");
 }
 };
 
-var flipCard = function (cardId) {
+var flipCard = function () {
+var cardId = this.getAttribute("data-id");
 console.log("User flipped " + cards[cardId].rank);
 console.log(cards[cardId].cardImage);
 console.log(cards[cardId].suit);
 cardsInPlay.push(cards[cardId].rank);
+this.setAttribute("src", cards[cardId].cardImage);
 if (cardsInPlay.length === 2) { 
 	if (cardsInPlay[0] === cardsInPlay[1]) { checkForMatch(); 
 		alert("You found a match!");
@@ -45,7 +47,14 @@ if (cardsInPlay.length === 2) {
 }
 };
 
-flipCard(0);
-flipCard(2);
+var createBoard = function () {
+	for (var i = 0; i < cards.length; i ++) {
+		var cardElement = document.createElement("img");
+		cardElement.setAttribute("src", "images/back.png");
+		cardElement.setAttribute("data-id", i);
+		cardElement.addEventListener("click", flipCard);
+		document.getElementById("game-board").appendChild(cardElement);
+	}
+};
 
-
+createBoard();
